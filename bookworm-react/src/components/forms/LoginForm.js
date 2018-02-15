8598f8d2af0c4c7c779543be8324dca85cb72996
Murray Catto <murraycatto@live.co.za>
2018-02-15 23:01:36 +0200

    add submit function to Login submit function and refactor it into a component

--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Form,Button} from "semantic-ui-react";
 import Validator from "validator";
+import PropTypes from "prop-types";
 import InlineError from "../messages/InlineError";
+
 class LoginForm extends React.Component {
 
   state = {
@@ -25,6 +27,9 @@ class LoginForm extends React.Component {
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
+    if(Object.keys(errors).length === 0){
+      this.props.submit(this.state.data);
+    }
   };
 
   validate = data =>{
@@ -39,7 +44,7 @@ class LoginForm extends React.Component {
     const { data, errors } = this.state;
     return (
       <Form onSubmit={this.onSubmit}>
-        <Form.Field>
+        <Form.Field error={!!errors.email}>
           <label  htmlFor="email">Email</label>
           <input
             type="email"
@@ -51,7 +56,7 @@ class LoginForm extends React.Component {
             />
         </Form.Field>
         {errors.email && <InlineError text={errors.email}/>}
-        <Form.Field>
+        <Form.Field error={!!errors.password}>
           <label  htmlFor="password">Password</label>
           <input
             type="password"
@@ -68,4 +73,9 @@ class LoginForm extends React.Component {
     );
   };
 };
+
+LoginForm.propTypes = {
+    submit: PropTypes.func.isRequired
+};
+
 export default LoginForm;
